72c35a202ff6ea3870b380c7944facca07651c14
CleoCTech <cleophasomwenga292@gmail.com>
2020-06-25 17:48:10 +0300

    Project Setup

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 text-center bg-dark">
+    <div class="container">
+      <span class="text-muted">Made with <span style="color: rgb(255,96,96)">❤</span> by <a href="#" target="_blank"> Max The Stranger</a>
+    </div>
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="continer">
+    About Page
+</div>
+@endsection
