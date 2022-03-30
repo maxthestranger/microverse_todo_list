7b0cbc326f4f7601d2a5a5a824149adba6453a08
maxthestranger <thestrangermax@gmail.com>
2022-03-30 13:29:30 +0300

    remove commented code

--- src/__tests__/tasks.test.js
+++ src/__tests__/tasks.test.js
@@ -1,10 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import { createTask, pushTask } from '../tasks.js';
-// import { tasksList } from './queries.js';
+import { pushTask } from '../tasks.js';
 
-// const updatedTaskList = tasksList.gettasksList();
 describe('AddingDescriptions', () => {
   test('list length to be 1', () => {
     document.body.innerHTML = `
@@ -14,8 +12,6 @@ describe('AddingDescriptions', () => {
     </div>
     `;
 
-    // const input = document.querySelector('.input');
-    // input.addEventListener('keyup', (e) => createTask(e));
     const ul = document.querySelector('.todo');
     const tasks = ['task 1', 'task 2', 'task 3', 'task 4'];
     for (let i = 0; i < tasks.length; i += 1) {
@@ -24,9 +20,4 @@ describe('AddingDescriptions', () => {
     const list = document.querySelectorAll('.todo li');
     expect(list).toHaveLength(tasks.length);
   });
-
-  //   let tasks = [];
-  //   for (let i = 0; i < 4; i++) {
-  //     tasks = pushTask('Wash dish', updatedTaskList.length + 1, false);
-  //   }
 });
